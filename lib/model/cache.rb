36a324655da72eacae114f1f1318e00b0213de06
hitode909 <hitode909@gmail.com>
2011-02-28 13:29:45 +0900

    support expire

--- lib/model/cache.rb
+++ lib/model/cache.rb
@@ -8,13 +8,13 @@ module Model
       MemCache.new('localhost:11211')
     end
 
-    def self.get_or_set(key)
+    def self.get_or_set(key, expire = 3600 * 24 *rand)
       raise "block needed" unless block_given?
+      key = key.to_s
       cache = self.instance.get(key)
       return cache if cache
 
       new_value = yield
-      expire = 3600 * 24 *rand
       self.instance.set(key, new_value, expire)
       new_value
     rescue => error
